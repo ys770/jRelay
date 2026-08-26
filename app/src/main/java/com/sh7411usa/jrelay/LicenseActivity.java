612ddcdd831e903d7da17b11240309ab96562ead
sh7411usa <sh7411usa@gmail.com>
2026-08-25 22:15:39 -0400

    Made webview links non clickable

--- app/src/main/java/com/sh7411usa/jrelay/LicenseActivity.java
+++ app/src/main/java/com/sh7411usa/jrelay/LicenseActivity.java
@@ -2,7 +2,9 @@ package com.sh7411usa.jrelay;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class LicenseActivity extends Activity {
 
@@ -12,6 +14,18 @@ public class LicenseActivity extends Activity {
         setContentView(R.layout.activity_license);
 
         WebView webView = findViewById(R.id.webview_license);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return true; // Block the navigation
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return true; // For older Android versions
+            }
+        });
         webView.loadUrl("file:///android_asset/license.html");
+
     }
 }
